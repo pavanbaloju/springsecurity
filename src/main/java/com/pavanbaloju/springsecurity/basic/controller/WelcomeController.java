22b3e59fbdda0f01b1d7ae9ed0a0678c73cbdf56
Pavan Baloju <pavanbaloju9@gmail.com>
2023-08-29 12:44:53 +0530

    [Add bank endpoints with default security]

--- src/main/java/com/pavanbaloju/springsecurity/basic/controller/WelcomeController.java
+++ src/main/java/com/pavanbaloju/springsecurity/basic/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.pavanbaloju.springsecurity.controller;
+package com.pavanbaloju.springsecurity.basic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
