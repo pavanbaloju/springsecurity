02c04a77cfebf8a8a0eb173e3472a359b4f87cf6
Pavan Baloju <pavanbaloju9@gmail.com>
2023-08-29 14:54:35 +0530

    ["Create in memory users"]

--- src/main/java/com/pavanbaloju/springsecurity/bank/controller/config/CustomSecurityConfig.java
+++ src/main/java/com/pavanbaloju/springsecurity/bank/controller/config/CustomSecurityConfig.java
@@ -3,6 +3,9 @@ package com.pavanbaloju.springsecurity.bank.controller.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,7 +14,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class CustomSecurityConfig {
 
     @Bean
-    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards").authenticated()
@@ -19,5 +22,22 @@ public class CustomSecurityConfig {
             .formLogin(withDefaults()) // to allow api requests with UI login
             .httpBasic(withDefaults()) // to allow api requests with login from say postman
             .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails adminUserDetails = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("12345")
+            .authorities("admin")
+            .build();
+
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("12345")
+            .authorities("read")
+            .build();
+
+        return new InMemoryUserDetailsManager(userDetails, adminUserDetails);
     }
 }
