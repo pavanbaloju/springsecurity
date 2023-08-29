9a9a7134b2ddecab01c8f416f29b477c880d41c6
Pavan Baloju <pavanbaloju9@gmail.com>
2023-08-29 13:06:41 +0530

    ["Add custom security config same as default config"]

---
+++ src/main/java/com/pavanbaloju/springsecurity/bank/controller/config/CustomSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.pavanbaloju.springsecurity.bank.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class CustomSecurityConfig {
+
+    @Bean
+    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+            .formLogin(withDefaults()) // to allow api requests with UI login
+            .httpBasic(withDefaults()) // to allow api requests with login from say postman
+            .build();
+    }
+}
