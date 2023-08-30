ee2f25619a7046e991eefb6b5d2f29506ff54bc8
Pavan Baloju <pavanbaloju9@gmail.com>
2023-08-30 11:14:46 +0530

    [Add bcrypt password encoder]

--- src/main/java/com/pavanbaloju/springsecurity/bank/service/LoginService.java
+++ src/main/java/com/pavanbaloju/springsecurity/bank/service/LoginService.java
@@ -2,18 +2,27 @@ package com.pavanbaloju.springsecurity.bank.service;
 
 import com.pavanbaloju.springsecurity.bank.entity.Customer;
 import com.pavanbaloju.springsecurity.bank.repository.CustomerRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public LoginService(CustomerRepository customerRepository) {
+    public LoginService(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createUser(Customer customer) {
+        hashPassword(customer);
         customerRepository.save(customer);
+    }
+
+    private void hashPassword(Customer customer) {
+        String hashedPwd = passwordEncoder.encode(customer.getPwd());
+        customer.setPwd(hashedPwd);
     }
 }
