a4db0a65bcb1338037e56e23a282e1a044caeaab
Pavan Baloju <pavanbaloju9@gmail.com>
2023-09-04 12:53:25 +0530

    ["Add custom filters for logging"]

--- src/main/java/com/pavanbaloju/springsecurity/SpringSecurityDemoApplication.java
+++ src/main/java/com/pavanbaloju/springsecurity/SpringSecurityDemoApplication.java
@@ -2,9 +2,8 @@ package com.pavanbaloju.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity(debug = false) //make true to debug filters
+//@EnableWebSecurity(debug = true) // to debug filters
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
